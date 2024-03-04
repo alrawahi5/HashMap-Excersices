a1bda769d8cbb05b1bf2f3eb7ae4689ebc1f26c8
ABDULLAH <abdullah555523@hotmail.com>
2024-03-04 11:56:17 +0400

    Added the iterator using keyset() hashmap task

---
+++ IterateOverHashMapkeySet.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class IterateOverHashMapkeySet {
+
+    public static void main(String[] args) {
+        HashMap<Integer, String> nameAndAge = new HashMap();
+        nameAndAge.put(771, "Abdullah");
+        nameAndAge.put(772, "Mohammed");
+        nameAndAge.put(773, "Noah");
+        nameAndAge.put(774, "Yousuf");
+        nameAndAge.put(775, "Anwar");
+
+        System.out.println(nameAndAge.entrySet());
+    }
+}
