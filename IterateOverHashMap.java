59a22f45d4a4c8d8aac39e394e3bcb8a365ffd35
ABDULLAH <abdullah555523@hotmail.com>
2024-03-04 11:47:45 +0400

    Added the iterator hashmap task

---
+++ IterateOverHashMap.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class IterateOverHashMap {
+
+    public static void main(String[] args) {
+        HashMap<Integer, String> nameAndAge = new HashMap();
+        nameAndAge.put(771, "Abdullah");
+        nameAndAge.put(772, "Mohammed");
+        nameAndAge.put(773, "Noah");
+        nameAndAge.put(774, "Yousuf");
+        nameAndAge.put(775, "Anwar");
+
+        nameAndAge.entrySet();
+
+        System.out.println(nameAndAge);
+    }
+}
