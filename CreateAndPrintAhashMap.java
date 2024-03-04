cb65f24c87ada6d5560f891f6dcfb3734932ed80
ABDULLAH <abdullah555523@hotmail.com>
2024-03-04 10:44:34 +0400

    Added the create and print a hashmap

---
+++ CreateAndPrintAhashMap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class CreateAndPrintAhashMap {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> nameAndAge = new HashMap();
+        nameAndAge.put("Abdullah", 25);
+
+        System.out.println(nameAndAge);
+    }
+}
